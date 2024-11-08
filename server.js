af54e41971626934ae635a081f2155d59fa0bdcc
Levin Mathew Abraham <levinabraham2819@gmail.com>
2024-11-08 11:39:54 +0530

    major update
    -added google authentication for sign in
    -improved sign in functionalities and front end
    -minor fixes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Route to serve the HTML file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, () => {
